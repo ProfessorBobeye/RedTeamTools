b7c970aae16f3d3c8500acecbff7cb3484b14d4e
Firat Acar <firat_097@hotmail.com>
2021-09-15 22:40:19 +0200

    Added resource file shellcode loading to freshycalls loader

--- ShellcodeLoader/Crypter/Crypter.h
+++ ShellcodeLoader/Crypter/Crypter.h
@@ -1,2 +1,4 @@
-void printShellcode(unsigned char* shellcode, size_t len);
-void xorCrypt(const char* key, int key_len, unsigned char* data, int data_len);
+#include <vector>
+
+void printShellcode(std::vector<unsigned char> shellcode, size_t len);
+std::vector<unsigned char> xorCrypt(const char* key, int key_len, std::vector<unsigned char> data, int data_len);
